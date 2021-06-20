1cee97ea0159dd2e87da97f96f5ee5bb79398c08
errorterror6 <81793788+errorterror6@users.noreply.github.com>
2021-06-20 10:47:44 +1000

    Update while_for_loops.py

--- while_for_loops.py
+++ while_for_loops.py
@@ -11,7 +11,7 @@ while a < b:   #this will repeat until the condition is no longer true
 
 print("""
 
-for loops
+---for loops---
 
    """)
 
@@ -20,5 +20,37 @@ a = 1
 
 for a in numbers:
     print(10*a)
+    
+print("""   
+
+--- maps ---
+
+   """)
+#Loops are often used to proccess every item in a list. e.g.
+#lets see if all strings in this list are numerical, using the default 
+#python function str.isnumeric
+numbers_2 = ["1","2","3","4","5","6"]
+
+#we could use:
+b = 0
+number_length = len(numbers_2)
+
+while b < number_length:
+    if str.isnumeric(numbers_2[b]):
+        print("Your string " + numbers_2[b] + " is numeric")
+    else:
+        print("illegal string")
+    b += 1
+
+#instead, we can map:
+#map(function,list)
+#basically applies the function to every member of the list.
+#this is very useful if your looping is very complex or long.
+
+if map(str.isnumeric,numbers_2) == False:
+    print("my string is illegal")
+else:
+    print("legal string")
+
 
 
