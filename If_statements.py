b395a4a9e6259eb8062176c1e7e783b083173d7e
errorterror6 <81793788+errorterror6@users.noreply.github.com>
2021-06-22 18:31:13 +1000

    Update If_statements.py

--- If_statements.py
+++ If_statements.py
@@ -1,3 +1,5 @@
+print("Welcome")
+
 is_male = True #boolean
 is_tall = False
 
@@ -13,30 +15,3 @@ elif is_male and not(is_tall):
 else:
     print("you are either not male or not tall or both.")
 
-def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2>=num3:
-        return num2
-    else:
-        return num3
-
-print(max_num(4,5,6))
-
-# def max_num(nums):  #equivalent to def max_num(num1, num2, num3):
-#     num1, num2, num3 = nums
-#     if num1 >= num2 and num1 >= num3:
-#         return num1
-#     elif num2 >= num1 and num2>=num3:
-#         return num2
-#     else:
-#         return num3
-
-# _456 = [4,5,6] 
-# print(max_num(_456)) #our function takes one argument so we have to give it one. 
-
-# num = [[1,3,4],[4,7,6],[2,4,5]]
-# cube_list = map(max_num, num) #map can only enter a single argument
-
-# print(cube_list)
-# print(list(cube_list))
